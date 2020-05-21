fc63c7c9c47b2a74a7052af08ef48a12ea19e7d8
Blissh <anthony.aka21@gmail.com>
2020-05-20 21:21:47 -0500

    Server done

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
+
+// Initializations
+const app = express();
+const port = 3000;
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir:path.join(app.get('views'), 'partials'),
+    extname:'.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'bandito',
+    resave: true,
+    saveUninitialized:true
+}))
+
+// Global variables
+
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/myrecipes'));
+app.use(require('./routes/recipes'));
+app.use(require('./routes/users'));
+
+
+// Static files 
+app.use(express.static(path.join(__dirname, 'public')));
+// Server listenning
+app.listen(port, () => console.log(`Example app listening on port`, port));
---
+++ src/routes/myrecipes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+router.get('/myrecipes', (req, res) => {
+  res.send('GET request to my recipes')
+});
+
+module.exports = router;
